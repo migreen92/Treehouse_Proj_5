6b1a78cc5b6c4d869799fa19a22dd1a7c009417a
Matt Green <81880241+migreen92@users.noreply.github.com>
2021-08-18 17:16:01 -0400

    Added JS, edited alt text

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 baguetteBox.run('.gallery');
+
+function imgSearch() {
+  let input = document.getElementById('img-search').value;
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('image');
+
+  for (i = 0; i < x.length; i++) {
+    if( !x[i].innerHTML.toLowerCase().includes(input)) {
+      x[i].style.display = "none";
+      } else {
+        x[i].style.display = "grid";
+      }
+    }
+}
